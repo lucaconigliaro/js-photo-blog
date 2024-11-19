9726c7635c753fd61cfeadf305eea5f96cff8545
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-19 15:26:04 +0100

    Milestone 2 and 3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 // **CONSEGNA**
-// **Milestone 1**
-// Sfruttando gli screen e gli asset in allegato riproduciamo la grafica proposta in maniera statica: utilizzando soltanto HTML e CSS e riproducendo una singola fotografia (usiamo una qualunque immagine a piacimento)
-// **Milestone 2**
-// Utilizzando Postman, testiamo una chiamata all’endpoint di JSON Placeholder:
-// https://jsonplaceholder.typicode.com/photos?_limit=6
-// Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come poterli sfruttare.
-// **Milestone 3**
-// Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSON Placeholder, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
 // **Bonus**
 // rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
+
+const rowElem = document.querySelector(".row");
+let photosArray = [];
+
+
+const printPost = () => {
+    photosArray.forEach((curItem) => {
+        rowElem.innerHTML += `
+             <div class="col">
+                    <div class="card-photo">
+                        <div class="photo">
+                            <img class="img-card" src="${curItem.thumbnailUrl}">
+                        </div>
+                        <div class="card-text">${curItem.title}</div>
+                    </div>
+    `
+    });
+}
+
+// Axios
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((resp) => {
+    photosArray = resp.data;
+    // console.log(photosArray);
+    printPost();
+});
