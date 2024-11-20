04418d049e99248cec4030a5e64fe9cf4f8ec8b2
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-20 14:58:32 +0100

    Milestone 3 problem with overlay

--- js/script.js
+++ js/script.js
@@ -1,23 +1,58 @@
-const rowElem = document.querySelector(".row");
+const rowElem = document.querySelector(".m-row");
+const overlayElem = document.getElementById("overlay");
+const overlayBtn = document.getElementById("overlay-btn");
+const imgCurElem = document.querySelector("img-cur");
 let photosArray = [];
 
-const printPost = () => {
+const printPhotos = () => {
+    let result = "";
     photosArray.forEach((curItem) => {
-        rowElem.innerHTML += `
-             <div class="col">
-                    <div class="card-photo">
-                        <div class="photo">
-                            <img class="img-card" src="${curItem.thumbnailUrl}">
-                        </div>
-                   <div class="card-text">${curItem.title}</div>
-             </div>
-    `
+       result += `
+            <div class="m-col">
+                <div class="m-card-photo" data-post-id="${curItem.id}">
+                    <div class="m-photo">
+                        <img src="${curItem.url}">
+                    </div>
+                    <div class="m-card-text">${curItem.title}</div>
+                </div>
+            </div>
+            `;
     });
+    rowElem.innerHTML = result
 }
+
+
+// Mostro overlay
+const showOverlay = (clickElem) => {
+    const postId = clickElem.dataset.postId;
+    console.log(postId);
+    const imgElem = photosArray.find((curItem) => curItem.id === postId);
+    console.log(imgElem);
+    overlayElem.classList.remove("d-none");
+    overlayElem.classList.add("d-flex");
+};
+
+// Nascondo overlay
+const hideOverlay = () => {
+    overlayElem.classList.remove("d-flex");
+    overlayElem.classList.add("d-none");
+};
+
+// Event listener
+const addEvent = () => {
+    const photos = document.querySelectorAll(".m-card-photo");
+    photos.forEach((curPhotos) => {
+        curPhotos.addEventListener("click", () => showOverlay(curPhotos));
+    });
+};
+
 
 // Axios
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((resp) => {
     photosArray = resp.data;
     // console.log(photosArray);
-    printPost();
+    printPhotos();
+    addEvent();
 });
+
+overlayBtn.addEventListener("click", hideOverlay);
